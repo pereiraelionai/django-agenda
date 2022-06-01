f1712f094958b3dd5ce3bb68925dfacbc0104f17
pereiraelionai <pereira.elionai92@gmail.com>
2022-05-31 22:42:50 -0300

    realizando melhorias no projeto, adicionando campo busca e caixa de menssagens

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+from django.contrib import messages
 
 
 def index(request):
+
     # contatos_bd = Contato.objects.all()
     contatos_bd = Contato.objects.order_by('-id') # adicionando um sinal de - ele ordena por ordem decrescente
     # contatos_bd = Contato.objects.order_by('-id').filter(
@@ -41,4 +45,40 @@ def ver_contato(request, contato_id): # contato id veio do urls (<int:contato_id
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        # raise Http404()
+        messages.add_message(request, messages.ERROR, 'Campo pesquisa não pode estar vazio.')
+        return redirect('index')
+
+    campos = Concat('nome', Value(' '),  'sobrenome')
+
+    contatos_bd = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) |
+        Q(telefone__icontains=termo)
+    )
+
+    '''
+    contatos_bd = Contato.objects.order_by('-id').filter(
+        Q(nome__icontains=termo) |
+        Q(sobrenome__icontains=termo)
+    )
+    '''
+
+    # print(contatos_bd.query)
+
+    paginator = Paginator(contatos_bd, 5)
+
+    page = request.GET.get('p')
+    contatos_bd = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'chave_contatos': contatos_bd
     })
--- projeto_agenda/settings.py
+++ projeto_agenda/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -121,7 +121,22 @@ STATICFILES_DIRS = [
     BASE_DIR / 'templates/static'
 ]
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Mensagens
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-sucess',
+    constants.INFO: 'alert-info',
+}
