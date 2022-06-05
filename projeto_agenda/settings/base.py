5224d081868f2f35d0cda81eebd8b1941e9bb5e3
pereiraelionai <pereira.elionai92@gmail.com>
2022-06-05 10:11:44 -0300

    inseriando arquivos para deploy heroku

--- projeto_agenda/settings/base.py
+++ projeto_agenda/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static'
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFileStorage"
+
+
+# STATIC_ROOT = BASE_DIR / 'static' # para fazer deploy é necessário coletar os staticos
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
--- projeto_agenda/wsgi.py
+++ projeto_agenda/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_agenda.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_agenda.settings.base')
 
 application = get_wsgi_application()
